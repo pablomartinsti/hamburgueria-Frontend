763e1e58ed85de7e3b65db2cc06d2857560551ea
pablomartinsti <pablomartinsti@outlook.com>
2024-04-18 15:23:55 -0300

    comit

---
+++ script.js
@@ -0,0 +1,90 @@
+const list = document.querySelector('ul')
+const linkFOREACH = document.querySelector('.FOREACH')
+const linkMAP = document.querySelector('.MAP')
+const linkREDUCE = document.querySelector('.REDUCE')
+const linkFILTER = document.querySelector('.FILTER')
+
+
+function formatMoeda(value) {
+
+    const newvalue = new Intl.NumberFormat('pt-br', 
+    { style: 'currency', 
+    currency: 'BRL' }).format(value)
+
+    return newvalue
+        
+}
+
+function showScreen(ARRAY) {
+
+
+    let myprodutc = ''
+
+    ARRAY.forEach((item) => {
+
+        myprodutc += `
+
+    
+    <li>
+
+         <img class="cardapio" src=${item.src}>
+
+         <p>${item.name}</p>
+
+         <h4>${formatMoeda(item.price)}</h4>
+    </li>
+    
+    `
+    })
+
+    list.innerHTML = myprodutc
+
+}
+function showDiscount() {
+
+    const discount = menu.map((value) => ({
+        ...value,
+        price: value.price * 0.9,
+
+    }))
+
+    showScreen(discount)
+
+}
+
+function showSum() {
+
+    const Sum = menu.reduce((acumulador, value) => acumulador + value.price, 0)
+
+    list.innerHTML = `
+    <li>
+
+
+    <h1> Valor Total do Pedido </h1> 
+    <h2> ${formatMoeda(Sum)}</h2>
+</li>`
+
+}
+
+function showFilter() {
+
+    const screenFilter = menu.filter((produtc) => produtc.lanches === false)
+
+    showScreen(screenFilter)
+
+}
+
+linkFOREACH.addEventListener('click', () => showScreen(menu))
+linkMAP.addEventListener('click', showDiscount)
+linkREDUCE.addEventListener('click', showSum)
+linkFILTER.addEventListener('click', showFilter)
+
+
+
+
+
+
+
+
+
+
---
+++ scriptprodutos.js
@@ -0,0 +1,21 @@
+const menu = [
+    { name: 'Big Burger', price: 36.99, lanches: true, src: './assets/1.png' },
+    { name: 'Big Beef Burge', price: 30.99, lanches: true, src: './assets/2.png' },
+    { name: 'Spicy Burger', price: 32.99, lanches: true, src: './assets/3.png' },
+    { name: 'Small Burger', price: 28.99, lanches: true, src: './assets/4.png' },
+    { name: 'X-Burger', price: 29.99, lanches: true, src: './assets/5.png' },
+    { name: 'X-Cheese', price: 34.99, lanches: true, src: './assets/6.png' },
+    { name: 'Coca Cola', price: 4.99, lanches: false, src: './assets/coca.png' },
+    { name: 'Coca Zero', price: 5.99, lanches: false, src: './assets/cocazero.png' },
+    { name: 'Fanta Guarana', price: 4.99, lanches: false, src: './assets/fantaguarana.png' },
+    { name: 'Fanta Uva', price: 4.99, lanches: false, src: './assets/fantauva.png' },
+    { name: 'Soda', price: 4.99, lanches: false, src: './assets/soda.png' },
+    { name: 'Sprite', price: 4.99, lanches: false, src: './assets/sprite.png' },
+    { name: 'Cebola Empanada', price: 17.99, porcoes: true, src: './assets/cebola.png' },
+    { name: 'mandioca', price: 14.99, procoes: true, src: './assets/mandioca.png' },
+    { name: 'Batata Frita', price: 19.99, porcoes: true, src: './assets/batata.png' },
+
+]
+
+
+
